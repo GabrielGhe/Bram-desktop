609154a783aa001ab31e7739f2b3c7d5194056d8
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-08-15 10:09:08 +0800

    Modified the list

--- app/components/DeckList/DeckList.js
+++ app/components/DeckList/DeckList.js
@@ -29,7 +29,6 @@ const DeckList = React.createClass({
   },
 
   _renderRow: function(rowData, sectionID, rowID, highlightRow) {
-    console.log("rowData:" + rowData + " sectionID:" + sectionID + " rowID:" + rowID + " highlightRow:" + highlightRow);
     return (
       <View style={styles.row}>
         <Text>{rowData}</Text>
