8dba778c22cc64803e7ae1ee12f18d6d23b66bc4
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-08-16 11:21:27 +0800

    updated card reducer

---
+++ app/reducers/card.js
@@ -0,0 +1,25 @@
+import Actions from '../actions/card';
+
+const initialState = {
+  cards: []
+}
+
+
+function addCard(state, action) {
+  
+}
+
+
+function removeCard(state, action) {
+  
+}
+
+
+function updateCard(state, action) {
+  
+}
+
+
+export default function reduceCard(state = initialState, action) {
+  
+}
