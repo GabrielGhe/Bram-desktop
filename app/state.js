31e69ff1aaf1720bbb45fc21521f72dc3475c64a
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2016-08-21 21:17:10 +0800

    made the state, kind of

--- app/state.js
+++ app/state.js
@@ -1,1 +1,49 @@
+export default state = {
+  isSearching: false,
+  isSearchingBy: "",
 
+  isLearning: true,
+  isLearningWithDeckId: 0,
+
+  decks: [{
+    id: 1,
+    text: 'one'
+  }, {
+    id: 2,
+    text: 'two'
+  }]
+
+  // decks: { 
+  //   '123g': {
+  //     id: '123g',
+  //     cards: [1, 2, 3, 4]
+  //   }, 
+
+  //   '243a': {
+  //     id: '243a',
+  //     cards: [5, 6, 7, 8]
+  //   }
+  // },
+
+  // cards: {
+  //   'sadf': {
+  //     id: 'sadf'
+  //     sides: [{
+  //       text: 'side 1'
+  //     }, {
+  //       text: 'side 2'
+  //     }]
+  //   },
+
+  //   'gneoir': {
+  //     id: 'gneoir',
+  //     sides: [{
+  //       text: 'side 1'
+  //     }, {
+  //       text: 'side 2'
+  //     }, {
+  //       text: 'side 3'
+  //     }]
+  //   }
+  // }
+};
